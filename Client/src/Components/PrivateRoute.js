32f5249a39a2b8d67cb4a17ad6989f039dc771c5
sadhik-a <sadhik@bititude.com>
2023-10-05 17:49:01 +0530

    added private routes

--- Client/src/Components/PrivateRoute.js
+++ Client/src/Components/PrivateRoute.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import LoginForm from "./Login/Login";
 const PrivateRoute = ({ element }) => {
+    console.log(element);
   const decodedTokenJSON = localStorage.getItem("decodedToken");
   const isAuthenticated = !!decodedTokenJSON;
     if (!isAuthenticated) {
