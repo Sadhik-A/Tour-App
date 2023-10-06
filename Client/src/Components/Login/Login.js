b3e468f79c7e633901e78d525e55294bbbd8f766
sadhik-a <sadhik@bititude.com>
2023-10-06 10:14:07 +0530

    private routes

--- Client/src/Components/Login/Login.js
+++ Client/src/Components/Login/Login.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import { useEffect,useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { setEmail, setPassword,submitLogin,setEmailError,setPasswordError, setLoading,setAlertMessage} from '../../redux/Userslice'; 
-import { Link, } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom'; 
-import './Loginform.css';
-  const decodedTokenJSON = localStorage.getItem("decodedToken");
-  const isAuthenticated = !!decodedTokenJSON;
+import React from "react";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  setEmail,
+  setPassword,
+  submitLogin,
+  setEmailError,
+  setPasswordError,
+  setLoading,
+  setAlertMessage,
+} from "../../redux/Userslice";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./Loginform.css";
+const decodedTokenJSON = localStorage.getItem("decodedToken");
+const isAuthenticated = !!decodedTokenJSON;
 function LoginForm() {
   const email = useSelector((state) => state.user.email);
   const password = useSelector((state) => state.user.password);
@@ -15,39 +23,37 @@ function LoginForm() {
   const alertMessage = useSelector((state) => state.user.alertmessage);
   const loading = useSelector((state) => state.user.loading);
   const dispatch = useDispatch();
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   const [localAlertMessage, setLocalAlertMessage] = useState("");
   const [localLoading, setLocalLoading] = useState(false);
   const [showpassword, setShowpassword] = useState(false);
   useEffect(() => {
-    if (isAuthenticated)
-      navigate('/home');
-  }, [isAuthenticated]);
-   useEffect(() => {
+    if (isAuthenticated) navigate("/home");
+  }, [navigate]);
+  useEffect(() => {
     return () => {
-      dispatch(setEmailError(''));
-      dispatch(setPasswordError(''));
-      dispatch(setEmail(''));
-      dispatch(setPassword(''));
+      dispatch(setEmailError(""));
+      dispatch(setPasswordError(""));
+      dispatch(setEmail(""));
+      dispatch(setPassword(""));
       dispatch(setLoading(false));
-      dispatch(setAlertMessage(''));
+      dispatch(setAlertMessage(""));
     };
-   }, [dispatch]);
-  
+  }, [dispatch]);
+
   useEffect(() => {
     setLocalAlertMessage(alertMessage);
   }, [alertMessage]);
-   useEffect(() => {
-   
+  useEffect(() => {
     if (alertMessage === "Login successful") {
-      setLocalAlertMessage('');
-      navigate('/home');
+      setLocalAlertMessage("");
+      navigate("/home");
     }
   }, [alertMessage, navigate]);
-   useEffect(() => {
-     setLocalLoading(loading);
-   }, [loading]);
-  
+  useEffect(() => {
+    setLocalLoading(loading);
+  }, [loading]);
+
   //  useEffect(() => {
   //    const storedToken = localStorage.getItem("authToken");
   //    if (storedToken) {
@@ -60,59 +66,55 @@ function LoginForm() {
     return emailRegex.test(email);
   };
   const validatePassword = (password) => {
-  return password.length >= 5;
+    return password.length >= 5;
   };
-    const togglePasswordVisibility = () => {
-      setShowpassword(!showpassword);
-    };
+  const togglePasswordVisibility = () => {
+    setShowpassword(!showpassword);
+  };
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    if (name === 'email') {
-      dispatch(setEmail(value)); 
-    
-    } else if (name === 'password') {
+    if (name === "email") {
+      dispatch(setEmail(value));
+    } else if (name === "password") {
       dispatch(setPassword(value));
-     
     }
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     let formErrors = {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     };
     if (!email) {
-      formErrors.email = 'Email is required';
-      dispatch(setEmailError('Email is required'));
+      formErrors.email = "Email is required";
+      dispatch(setEmailError("Email is required"));
     } else if (!validateEmail(email)) {
-      formErrors.email = 'Please enter a valid email';
-      dispatch(setEmailError('Please enter a valid email'));
-    }else{
-       dispatch(setEmailError(''));
+      formErrors.email = "Please enter a valid email";
+      dispatch(setEmailError("Please enter a valid email"));
+    } else {
+      dispatch(setEmailError(""));
     }
 
     if (!password) {
-    formErrors.password = 'Password is required';
-    dispatch(setPasswordError('Password is required'));
-  } else if (!validatePassword(password)) {
-    formErrors.password = 'Password must be at least 5 characters long';
-    dispatch(setPasswordError('Password must be at least 5 characters long'));
-  } else {
-    dispatch(setPasswordError('')); 
-  }
+      formErrors.password = "Password is required";
+      dispatch(setPasswordError("Password is required"));
+    } else if (!validatePassword(password)) {
+      formErrors.password = "Password must be at least 5 characters long";
+      dispatch(setPasswordError("Password must be at least 5 characters long"));
+    } else {
+      dispatch(setPasswordError(""));
+    }
 
-  if (!formErrors.email && !formErrors.password) {
-    dispatch(submitLogin({ email, password }));
-     dispatch(setEmail(""));
-     dispatch(setPassword(""));
-  }
+    if (!formErrors.email && !formErrors.password) {
+      dispatch(submitLogin({ email, password }));
+      dispatch(setEmail(""));
+      dispatch(setPassword(""));
+    }
   };
-  
-
 
   return (
-    <div  className="container" >
+    <div className="container">
       <div className="form-container">
         <h2 className="form-title">Login Form</h2>
         <form onSubmit={handleSubmit} className="form">
@@ -125,7 +127,8 @@ function LoginForm() {
               name="email"
               value={email}
               onChange={handleInputChange}
-              placeholder='@'/>
+              placeholder="@"
+            />
             <div className="error-container">
               {emailError && <span className="error">{emailError}</span>}
             </div>
@@ -140,11 +143,14 @@ function LoginForm() {
                 type={showpassword ? "text" : "password"}
                 name="password"
                 value={password}
-                onChange={(e) => dispatch(setPassword(e.target.value))}/>
+                onChange={(e) => dispatch(setPassword(e.target.value))}
+              />
               <i
                 className={`password-toggle-icon fas ${
-                  showpassword ? "fa-eye-slash" : "fa-eye"}`}
-                onClick={togglePasswordVisibility}></i>
+                  showpassword ? "fa-eye-slash" : "fa-eye"
+                }`}
+                onClick={togglePasswordVisibility}
+              ></i>
             </div>
             <div className="error-container">
               {passwordError && <span className="error">{passwordError}</span>}
@@ -153,17 +159,20 @@ function LoginForm() {
           <button
             type="submit"
             className="submit-button"
-            disabled={localLoading}>
+            disabled={localLoading}
+          >
             {localLoading ? "Logging in..." : "Login"}
           </button>
           {localAlertMessage && (
             <p
               className={`alert ${
-                localAlertMessage === "Login successful" ? "success" : "error"}`}>
+                localAlertMessage === "Login successful" ? "success" : "error"
+              }`}
+            >
               {localAlertMessage}
             </p>
           )}
-         
+
           <p>
             Don't have an account?{" "}
             <Link to="/register">
@@ -174,7 +183,6 @@ function LoginForm() {
       </div>
     </div>
   );
-
 }
 
 export default LoginForm;
