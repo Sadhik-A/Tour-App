89ee5bdbe090a8e002cffc35625f7179e91facaa
sadhik-a <sadhik@bititude.com>
2023-10-10 11:39:07 +0530

    alert dialog

--- Client/src/Components/ConfirmDialog/ConfirmDialog.js
+++ Client/src/Components/ConfirmDialog/ConfirmDialog.js
@@ -1,15 +1,14 @@
-
 import React from "react";
 import "./ConfirmDialog.scss";
 
 function ConfirmDialog({ title, subtitle, onConfirm, onCancel }) {
-    console.log(onConfirm);
+  console.log(onConfirm);
   return (
     <div className="confirm-dialog-container">
       <div className="confirm-dialog">
         <h3>{title}</h3>
         <p>{subtitle}</p>
-        <button onClick={onConfirm}>Yes</button>
+        <button onClick={() => onConfirm}>Yes</button>
         <button onClick={onCancel}>No</button>
       </div>
     </div>
