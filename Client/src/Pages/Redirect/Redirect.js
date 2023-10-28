fb5dd2a857cfd5d54f8e14de6c70f3160a825177
sadhik-a <sadhik@bititude.com>
2023-10-28 20:42:56 +0530

    google login completed

--- Client/src/Pages/Redirect/Redirect.js
+++ Client/src/Pages/Redirect/Redirect.js
@@ -1,28 +1,31 @@
-import React , { useEffect } from 'react';
-import { GoogleLogin } from '../../redux/Userslice';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { GoogleLogin } from "../../redux/Userslice";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-
 
 function Redirect() {
   const user = useSelector((state) => state.user);
-  console.log(user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
   useEffect(() => {
-    dispatch(GoogleLogin());
-    if (user) {
-      navigate("/landing");
-    }
-    else {
-      navigate("/");
-    }
-  }, [dispatch,navigate,user]); // Empty dependency array to run the effect once
+    const fetchData = async () => {
+      await dispatch(GoogleLogin());
+      if (user) {
+        navigate("/landing");
+      } else {
+        navigate("/");
+      }
+    };
+    fetchData();
+  }, [dispatch, navigate, user]);
 
+  // Conditional rendering: Display loading message or any other UI while fetching data
+  if (user === null) {
+    return <div>Loading...</div>;
+  }
 
-  return (
-    <div>Redirect</div>
-  );
+  return <div>Redirect</div>;
 }
 
 export default Redirect;
