26643ebbc05c1f16a8d5d732e5d6d35cbe5abf43
sadhik-a <sadhik@bititude.com>
2023-10-28 20:33:09 +0530

    google login completed

--- Client/src/Pages/Redirect/Redirect.js
+++ Client/src/Pages/Redirect/Redirect.js
@@ -1,16 +1,22 @@
-import React from 'react';
-// import { GoogleLogin } from '../../redux/Userslice';
-// import { useDispatch } from 'react-redux';
-// import { useNavigate } from "react-router-dom";
+import React , { useEffect } from 'react';
+import { GoogleLogin } from '../../redux/Userslice';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from "react-router-dom";
 
 
 function Redirect() {
-  // const navigate = useNavigate();
-  // const dispatch = useDispatch();
-  // useEffect(() => {
-  //   dispatch(GoogleLogin());
-  //   navigate("/landing");
-  // }, [dispatch,navigate]); // Empty dependency array to run the effect once
+  const user = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(GoogleLogin());
+    if (user) {
+      navigate("/landing");
+    }
+    else {
+      navigate("/");
+    }
+  }, [dispatch,navigate]); // Empty dependency array to run the effect once
 
 
   return (
